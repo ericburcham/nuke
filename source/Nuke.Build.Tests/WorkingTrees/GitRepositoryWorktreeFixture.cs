63420df64c91155cd880f093d90ddc1d6c52ec14
Eric Burcham <eburcham@eprod.com>
2024-03-04 17:59:03 -0600

    add failing tests:  GitRepositoryWorktreeTests

---
+++ source/Nuke.Build.Tests/WorkingTrees/GitRepositoryWorktreeFixture.cs
@@ -0,0 +1,173 @@
+﻿// Copyright 2024 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Nuke.Common.Git;
+using Nuke.Common.IO;
+
+namespace Nuke.Common.Tests;
+
+public sealed class GitRepositoryWorktreeFixture : IDisposable
+{
+    /// <summary>
+    ///     The methods performs the arrange and act steps for the tests.  The Facts perform the assertions.
+    /// </summary>
+    /// <remarks>
+    ///     The setup steps are:
+    ///     1.  Determine the git directory for the current test run.
+    ///     2.  Create a new working tree at ./git../../nuke-test-worktree with a new branch name (random to avoid possible
+    ///     collisions)
+    ///     3.  Open a new GitRepositoryAttribute instance of the working tree using GitRepository.FromLocalDirectory(...);
+    /// </remarks>
+    public GitRepositoryWorktreeFixture()
+    {
+        // This is the build's current directory.
+        var currentDirectory = Directory.GetCurrentDirectory();
+
+        // Use the GitRepository class to get the root of the git repository.  We need this because our worktree needs to live outside the main
+        // working tree.
+        MainWorktree = GitRepository.FromLocalDirectory(currentDirectory).NotNull();
+        var gitDirectory = MainWorktree.LocalDirectory;
+        var gitDirectoryName = gitDirectory.Name;
+
+        // This is where we will create our test working tree.  This is a field because we need to clean it up.
+        WorktreeDirectory = gitDirectory / .. / $"{gitDirectoryName}-test-worktree";
+
+        // Remove the working tree destination if it exists.  It shouldn't but this will make things run more smoothly.
+        DeleteTestWorktreeDirectory();
+
+        // Prune any orphaned working trees.
+        PruneOrphanedWorktrees();
+
+        // This is the new branch name we'll use.  This is a field because we need to clean it up.
+        var branchGuid = Guid.NewGuid().ToString().Replace("-", string.Empty);
+        BranchName = $"test-worktree-{branchGuid}";
+
+        // Create the test worktree.
+        CreateTestWorktree();
+    }
+
+    ~GitRepositoryWorktreeFixture()
+    {
+        DisposeInternal();
+    }
+
+    public void Dispose()
+    {
+        DisposeInternal();
+        GC.SuppressFinalize(this);
+    }
+
+    internal string BranchName { get; }
+
+    internal GitRepository MainWorktree { get; }
+
+    internal AbsolutePath WorktreeDirectory { get; }
+
+    private void CreateTestWorktree()
+    {
+        var createWorktree = new Process { StartInfo = ConfigureProcessStartInfo("git.exe", $"worktree add {WorktreeDirectory} -b {BranchName}") };
+        createWorktree.Start();
+        createWorktree.WaitForExit();
+        if (createWorktree.ExitCode != 0)
+        {
+            var createWorktreeError = createWorktree.StandardError.ReadToEnd();
+            var failureMessage = $"Creating the test worktree failed with error message:  {createWorktreeError}";
+            throw new InvalidOperationException(failureMessage);
+        }
+    }
+
+    private void PruneOrphanedWorktrees()
+    {
+        var pruneOrphanedWorktrees = new Process { StartInfo = ConfigureProcessStartInfo("git.exe", "worktree prune") };
+        pruneOrphanedWorktrees.Start();
+        pruneOrphanedWorktrees.WaitForExit();
+        if (pruneOrphanedWorktrees.ExitCode != 0)
+        {
+            var pruneOrphanedWorktreesError = pruneOrphanedWorktrees.StandardError.ReadToEnd();
+            var failureMessage = $"Pruning orphaned working trees failed with error message:  {pruneOrphanedWorktreesError}";
+            throw new InvalidOperationException(failureMessage);
+        }
+    }
+
+    private void DeleteTestWorktreeDirectory()
+    {
+        if (Directory.Exists(WorktreeDirectory))
+        {
+            Directory.Delete(WorktreeDirectory, recursive: true);
+        }
+    }
+
+    private ProcessStartInfo ConfigureProcessStartInfo(string fileName, string arguments)
+    {
+        return new ProcessStartInfo(fileName, arguments)
+               {
+                   RedirectStandardError = true,
+                   RedirectStandardOutput = true
+               };
+    }
+
+    private void RemoveTestBranch()
+    {
+        var removeBranch = new Process { StartInfo = ConfigureProcessStartInfo("git.exe", $"branch -D {BranchName}") };
+        removeBranch.Start();
+        removeBranch.WaitForExit();
+        if (removeBranch.ExitCode != 0)
+        {
+            var removeBranchError = removeBranch.StandardError.ReadToEnd();
+            var failureMessage = $"Removing the test branch ({BranchName}) failed with error message:  {removeBranchError}";
+            throw new InvalidOperationException(failureMessage);
+        }
+    }
+
+    private void RemoveTestWorktree()
+    {
+        var removeWorktree = new Process { StartInfo = ConfigureProcessStartInfo("git.exe", $"worktree remove {WorktreeDirectory} --force --force") };
+        removeWorktree.Start();
+        removeWorktree.WaitForExit();
+        if (removeWorktree.ExitCode != 0)
+        {
+            var removeWorktreeError = removeWorktree.StandardError.ReadToEnd();
+            var failureMessage = $"Removing the test worktree failed with error message:  {removeWorktreeError}";
+            throw new InvalidOperationException(failureMessage);
+        }
+    }
+
+    private void DisposeInternal()
+    {
+        try
+        {
+            RemoveTestWorktree();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            // Swallow exceptions.
+        }
+
+        try
+        {
+            RemoveTestBranch();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            // Swallow exceptions.
+        }
+
+        // Remove the working tree destination if it still exists.  It shouldn't but this will make things run more smoothly.
+        try
+        {
+            DeleteTestWorktreeDirectory();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            // Swallow exceptions.
+        }
+    }
+}
---
+++ source/Nuke.Build.Tests/WorkingTrees/GitRepositoryWorktreeTests.cs
@@ -0,0 +1,126 @@
+﻿// Copyright 2024 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using System;
+using System.Linq;
+using FluentAssertions;
+using Nuke.Common.Git;
+using Xunit;
+
+namespace Nuke.Common.Tests;
+
+/// <summary>
+///     Make assertions about the GitRepositoryAttribute's ability to deal with git working trees.
+/// </summary>
+/// <remarks>
+///     Since GitRepositoryAttribute defers to GitRepository.FromLocalDirectory, we will just test that method
+///     directly for the time being.  As an improvement, we should test GitRepositoryAttribute.GetValue(...) instead.
+/// </remarks>
+public sealed class GitRepositoryWorktreeTests : IClassFixture<GitRepositoryWorktreeFixture>
+{
+    private readonly Lazy<GitRepository> _testWorkTree;
+
+    private readonly GitRepositoryWorktreeFixture _fixture;
+
+    /// <summary>
+    ///     The methods performs the arrange and act steps for the tests.  The Facts perform the assertions.
+    /// </summary>
+    /// <remarks>
+    ///     The setup steps are:
+    ///     1.  Determine the git directory for the current test run.
+    ///     2.  Create a new working tree at ./git../../nuke-test-worktree with a new branch name (random to avoid possible
+    ///     collisions)
+    ///     3.  Open a new GitRepositoryAttribute instance of the working tree using GitRepository.FromLocalDirectory(...);
+    /// </remarks>
+    public GitRepositoryWorktreeTests(GitRepositoryWorktreeFixture fixture)
+    {
+        _fixture = fixture;
+        _testWorkTree = new Lazy<GitRepository>(LoadTestWorkTree);
+    }
+
+    private GitRepository TestWorkTree => _testWorkTree.Value;
+
+    [Fact]
+    public void RepositoryShouldNotBeNull()
+    {
+        TestWorkTree.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void RepositoryShouldHaveTheCorrectBranch()
+    {
+        TestWorkTree.Branch.Should().Be(_fixture.BranchName);
+    }
+
+    [Fact]
+    public void RepositoryShouldHaveTheCorrectCommit()
+    {
+        TestWorkTree.Commit.Should().NotBe(_fixture.MainWorktree.Commit).And.NotBeNull();
+    }
+
+    [Fact]
+    public void RepositoryShouldHaveTheCorrectEndpoint()
+    {
+        TestWorkTree.Endpoint.Should().Be(_fixture.MainWorktree.Branch);
+    }
+
+    [Fact]
+    public void RepositoryShouldHaveTheCorrectHead()
+    {
+        TestWorkTree.Head.Should().NotBe(_fixture.MainWorktree.Head).And.NotBeNull();
+    }
+
+    [Fact]
+    public void RepositoryShouldHaveTheCorrectHttpsUrl()
+    {
+        TestWorkTree.HttpsUrl.Should().Be(_fixture.MainWorktree.HttpsUrl);
+    }
+
+    [Fact]
+    public void RepositoryShouldHaveTheCorrectIdentifier()
+    {
+        TestWorkTree.Identifier.Should().Be(_fixture.MainWorktree.Identifier);
+    }
+
+    [Fact]
+    public void RepositoryShouldHaveTheCorrectLocalDirectory()
+    {
+        TestWorkTree.LocalDirectory.Should().Be(_fixture.WorktreeDirectory);
+    }
+
+    [Fact]
+    public void RepositoryShouldHaveTheCorrectProtocol()
+    {
+        TestWorkTree.Protocol.Should().Be(_fixture.MainWorktree.Protocol);
+    }
+
+    [Fact]
+    public void RepositoryShouldHaveTheCorrectRemoteBranch()
+    {
+        TestWorkTree.RemoteBranch.Should().BeNull();
+    }
+
+    [Fact]
+    public void RepositoryShouldHaveTheCorrectRemoteName()
+    {
+        TestWorkTree.RemoteName.Should().BeNull();
+    }
+
+    [Fact]
+    public void RepositoryShouldHaveTheCorrectSshUrl()
+    {
+        TestWorkTree.SshUrl.Should().Be(_fixture.MainWorktree.SshUrl);
+    }
+
+    [Fact]
+    public void RepositoryShouldHaveTheCorrectTags()
+    {
+        TestWorkTree.Tags.Should().Equal(_fixture.MainWorktree.Tags);
+    }
+
+    private GitRepository LoadTestWorkTree()
+    {
+        return GitRepository.FromLocalDirectory(_fixture.WorktreeDirectory).NotNull();
+    }
+}
